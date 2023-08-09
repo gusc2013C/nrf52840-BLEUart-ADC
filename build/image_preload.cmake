c2830234ce6888e9252aae09cf0a8f37417aeebb
sc gu <849843439@qq.com>
2023-08-10 07:56:58 +0800

    ignoring build folder will lead to unexpected issues

---
+++ build/image_preload.cmake
@@ -0,0 +1,8 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "d:/git-projects/nrf52840-bleuart-adc;C:/ncs/v2.4.1/nrf;C:/ncs/v2.4.1/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "d:/Git-Projects/nrf52840-BLEUart-ADC/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "D:/Git-Projects/nrf52840-BLEUart-ADC/app.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/31f4403e35/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
